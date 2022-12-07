b05d04285f140c91e251be68d4a60df12750b6f0
Mikołaj Wesołek <mikolajw31@wp.pl>
2022-12-07 19:04:28 +0100

    feat: creating debounce hook for character name filter

---
+++ src/hooks/useDebounce/useDebounce.ts
@@ -0,0 +1,33 @@
+import { useEffect, useRef, useState } from 'react'
+
+export const useDebounce = <T, F extends (...args: Parameters<F>) => ReturnType<F>>(
+  value: T,
+  debouncedFunction?: F,
+  delay = 300,
+) => {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value)
+  const isMounted = useRef(false)
+
+  useEffect((...args: Parameters<F>) => {
+    if (!isMounted.current) {
+      isMounted.current = true
+      return
+    }
+
+    const timer = setTimeout(() => {
+      if (debouncedFunction !== undefined) {
+        debouncedFunction(...args)
+      }
+
+      setDebouncedValue(value)
+    }, delay)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  },
+  [value],
+  )
+
+  return debouncedValue
+}
